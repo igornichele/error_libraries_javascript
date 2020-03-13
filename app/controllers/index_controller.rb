e54d29b2c6793550ef97395724160b6337e56ac9
Igor Nichele <>
2020-03-13 01:14:05 -0300

    init

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,6 @@
+class IndexController < ApplicationController
+
+    def page
+
+    end
+end
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,33 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+import $ from 'jquery';
+global.$ = jQuery;
+
+//require('alertifyjs/build/alertify.min.js');
+//require('alertifyjs/src/js/confirm.js');
+//require('alertifyjs/src/js/event.js');
+//require('alertifyjs/src/js/intro.js');
+//require('alertifyjs/src/js/notifier.js');
+//require('alertifyjs/src/js/outro.js');
+//require('alertifyjs/src/js/prompt.js');
+
+//require('alertifyjs');
+
+//require("moment/package.js");
+require("moment/moment.js");
+
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
